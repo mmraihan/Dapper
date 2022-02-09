7fcc9465f3e6bc5dad298e57360837996cfbd4f3
mmraihan <mm_raihan@live.com>
2022-02-10 01:25:21 +0600

    EF Core: Update Controller using IRepositoryCompany.

--- DapperDemo/DapperDemo/Startup.cs
+++ DapperDemo/DapperDemo/Startup.cs
@@ -1,4 +1,5 @@
 using DapperDemo.Data;
+using DapperDemo.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace DapperDemo
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<ICompanyRepository, CompanyRepositoryEF>();
             services.AddControllersWithViews();
         }
 
